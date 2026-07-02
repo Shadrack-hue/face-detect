812771fa30360d1d123d307df73044efd6bb4cfb
Shadrack Favour <shadracfavour44@gmail.com>
2026-07-02 17:18:16 +0300

    Refactor faceEngine.js to use common options

--- src/modules/faceEngine.js
+++ src/modules/faceEngine.js
@@ -8,23 +8,38 @@ export class FaceEngine {
 
     async initialize() {
         const filesetResolver = await FilesetResolver.forVisionTasks(
-            "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+            'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm'
         );
-        
-        this.landmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
-            baseOptions: {
-                modelAssetPath: "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task",
-                delegate: "GPU"
-            },
+
+        const commonOptions = {
             outputFaceBlendshapes: false,
             outputFacialTransformationMatrixes: false,
-            runningMode: "VIDEO",
+            runningMode: 'VIDEO',
             numFaces: 10,
             minFaceDetectionConfidence: 0.5,
             minFacePresenceConfidence: 0.5,
             minTrackingConfidence: 0.5
-        });
-        
+        };
+
+        try {
+            this.landmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
+                baseOptions: {
+                    modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task',
+                    delegate: 'GPU'
+                },
+                ...commonOptions
+            });
+        } catch (error) {
+            console.warn('GPU face detection delegate is unavailable, retrying with CPU.', error);
+            this.landmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
+                baseOptions: {
+                    modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task',
+                    delegate: 'CPU'
+                },
+                ...commonOptions
+            });
+        }
+
         return true;
     }
 
@@ -35,5 +50,7 @@ export class FaceEngine {
 
     start() { this.isRunning = true; }
     stop() { this.isRunning = false; }
+    toggle() { this.isRunning = !this.isRunning; return this.isRunning; }
+}    stop() { this.isRunning = false; }
     toggle() { this.isRunning = !this.isRunning; return this.isRunning; }
 }
